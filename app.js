484ba34ff715c68a9bcc021ad6bfbdd42c7c78cc
Anar Bayramov <anar.bayram@yahoo.com>
2024-03-16 02:34:37 +0400

    updated product routes

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use('/api/v1', productRoutes);
+app.use('/api/v1/products', productRoutes);
 
 app.use(errorHandler);
 
--- products/routes.js
+++ products/routes.js
@@ -4,8 +4,7 @@ const { getProducts, exportProducts } = require("./contollers");
 
 const router = Router();
 
-router.get('/products', getProducts);
-router.post('/products/export', exportProducts);
-
+router.get('/', getProducts);
+router.post('/export', exportProducts);
 
 module.exports = router;
